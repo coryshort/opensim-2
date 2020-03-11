d4cd7f913227b0aea2abd64b2be56f248708ee96
lickx <sinesurfer@gmail.com>
2020-03-11 11:15:24 +0100

    Missed file

--- OpenSim/Region/OptionalModules/World/NPC/NPCAvatar.cs
+++ OpenSim/Region/OptionalModules/World/NPC/NPCAvatar.cs
@@ -675,6 +675,10 @@ namespace OpenSim.Region.OptionalModules.World.NPC
         {
         }
 
+        public virtual void SendStartPingCheck(byte seq)
+        {
+        }
+
         public virtual void SendAvatarPickerReply(AvatarPickerReplyAgentDataArgs AgentData, List<AvatarPickerReplyDataArgs> Data)
         {
         }
