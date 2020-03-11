6e27f60880d608e0d0f71dcd0b91a43e354911fb
lickx <sinesurfer@gmail.com>
2020-03-11 09:07:38 +0100

    Update

--- OpenSim/Region/Framework/Scenes/SceneObjectGroup.cs
+++ OpenSim/Region/Framework/Scenes/SceneObjectGroup.cs
@@ -132,6 +132,8 @@ namespace OpenSim.Region.Framework.Scenes
         private long m_minPersistTime = 0;
 //        private Random m_rand;
 
+        public int PseudoCRC;
+
         /// <summary>
         /// This indicates whether the object has changed such that it needs to be repersisted to permenant storage
         /// (the database).
